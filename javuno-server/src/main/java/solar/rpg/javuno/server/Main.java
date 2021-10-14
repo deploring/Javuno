0987d9621500a36e687856d6d961d82f61ec92d4
Joshua <me@jss.id.au>
2021-10-14 23:56:09 +1100

    Update Main.java

--- javuno-server/src/main/java/solar/rpg/javuno/server/Main.java
+++ javuno-server/src/main/java/solar/rpg/javuno/server/Main.java
@@ -1,8 +1,29 @@
 package solar.rpg.javuno.server;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
+        SwingUtilities.invokeLater(() -> {
+            Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+            JFrame frame = new MainView(logger);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setMinimumSize(new Dimension(900, 700));
+            frame.setPreferredSize(new Dimension(900, 700));
+            frame.pack();
+        });
     }
 }
